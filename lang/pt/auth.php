9d1215f2a962477f9ba3967e6ad689b524b26f55
Nelson Alexandre Mutane <nelson.mutane@uzambeze.ac.mz>
2024-09-12 23:53:37 +0200

    dix laravel seo

--- lang/pt/auth.php
+++ lang/pt/auth.php
@@ -1,19 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed'   => 'As credencias indicadas não coincidem com as registadas no sistema.',
-    'throttle' => 'O número limite de tentativas de login foi atingido. Por favor tente novamente em :seconds segundos.',
-
+    'failed'   => 'Credenciais informadas não correspondem com nossos registros.',
+    'password' => 'A senha está incorreta.',
+    'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
 ];
